64b1e9522ba8838351f59698186a5b238b9d522f
Jackie Cohen <jczetta@gmail.com>
2012-02-06 16:00:21 -0500

    second commit, added very beginning of 99 problems working

---
+++ 99problems_v_2.hs
@@ -0,0 +1,14 @@
+--- bear with me. (jzc)
+
+--- attempts at 99 Haskell problems (http://www.haskell.org/haskellwiki/99_questions/1_to_10 etc)
+-- these won't have much of an order -- stuff I'm going through to get used to Haskell
+-- coming from OO-y Python and C++ 
+
+-- def'n of infix operator in Prelude:
+{-
+(!!) :: [a] -> Int -> a
+(x:_) !! 0 = x
+(_:xs) !! n = xs !! (n-1)
+-}
+
+

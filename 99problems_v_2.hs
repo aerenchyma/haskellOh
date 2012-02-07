750f2f13c5b9c3e107f0124f5cf3736b10bb9325
Jackie Cohen <jczetta@gmail.com>
2012-02-07 17:14:56 -0500

    having difficulty with why my isPal soln doesn't work (so far), looking at sol'ns

--- 99problems_v_2.hs
+++ 99problems_v_2.hs
@@ -15,3 +15,33 @@
 myLength :: [a] -> Int -- (err A:) doing [a] -> Int -> a   ==> this whole fxn won't compile
 myLength [] = 0
 myLength (_:xs) = 1 + (myLength xs) -- error msg will appear here (err A)
+
+-- reverse a list
+reverseList :: [a] -> [a]
+reverseList [] = []
+reverseList [x] = [x]
+reverseList (x:xs) = reverseList xs ++ [x]
+
+--- the less wasteful solution (per http://www.haskell.org/haskellwiki/99_questions/Solutions/5):
+reverseList' :: [a] -> [a]
+reverseList' list = reverseList' list []
+	where
+		reverseList' [] reversed = reversed
+		reverseList' (x:xs) reversed = reverseList' xs (x:reversed)
+
+-- find out whether a list is a palindrome
+isPal :: [a] -> Bool
+isPal [] = True
+isPal [_] = True
+isPal xs = (xs == (reverseList' xs)) -- what specifically is wrong with these things?
+	
+
+
+
+{-
+isPal (x:xs) = True
+	if
+		reverseList' (x:xs) = (x:xs)
+isPal (x:xs) = False
+	if reverseList' (x:xs) /= (x:xs)
+-}

f01e353b4a3974e65994adf4aaa40c3b849d080f
Jackie Cohen <jczetta@gmail.com>
2012-02-07 11:03:04 -0500

    realization of silly errors that can be made in Haskell which will prevent compiling

--- 99problems_v_2.hs
+++ 99problems_v_2.hs
@@ -2,7 +2,6 @@
 
 --- attempts at 99 Haskell problems (http://www.haskell.org/haskellwiki/99_questions/1_to_10 etc)
 -- these won't have much of an order -- stuff I'm going through to get used to Haskell
--- coming from OO-y Python and C++ 
 
 -- def'n of infix operator in Prelude:
 {-
@@ -12,3 +11,7 @@
 -}
 
 
+-- find the number of elements in a list:
+myLength :: [a] -> Int -- (err A:) doing [a] -> Int -> a   ==> this whole fxn won't compile
+myLength [] = 0
+myLength (_:xs) = 1 + (myLength xs) -- error msg will appear here (err A)
